8a14c702a8e4cd87c815a96d13f0634151a12e54
sevki <s@sevki.org>
2020-10-31 18:46:45 +0000

    oututil: add example

--- oututil/debug_test.go
+++ oututil/debug_test.go
@@ -2,6 +2,7 @@ package oututil
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -25,4 +26,13 @@ func ExamplePrefixLine() {
 	}
 	b := bytes.NewBuffer(dump)
 	PrefixLine(b, "> ", 1)
+	fmt.Println(b.String())
+	// Output:
+	// >  PUT / HTTP/1.1
+	// >  Host: www.example.org
+	// >  User-Agent: Go-http-client/1.1
+	// >  Content-Length: 75
+	// >  Accept-Encoding: gzip
+	// >
+	// >  Go is a general-purpose language designed with systems programming in mind.
 }
